5389950d333da1a9a2c985c2aae3d055d4aaa4d5
Usuário <Usurio@DESKTOP-S6UICUT>
2020-02-08 13:26:17 -0200

    herança

--- src/Empregado.java
+++ src/Empregado.java
@@ -1,25 +1,11 @@
 
+
 public class Empregado {
 
-    private int tipo;
     private double salario;
-    private double percentualComissao;
-    private double bonus;
 
     public Empregado() {
 
-    }
-
-    public Empregado(int tipo) {
-        this.tipo = tipo;
-    }
-
-    public int getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(int tipo) {
-        this.tipo = tipo;
     }
 
     public double getSalario() {
@@ -30,36 +16,8 @@ public class Empregado {
         this.salario = salario;
     }
 
-    public double getPercentualComissao() {
-        return percentualComissao;
-    }
-
-    public void setPercentualComissao(double percentualComissao) {
-        this.percentualComissao = percentualComissao;
-    }
-
-    public double getBonus() {
-        return bonus;
-    }
-
-    public void setBonus(double bonus) {
-        this.bonus = bonus;
-    }
-    
-    /*public void salarioFinal(int tipo){
-        double salarioFinal = 0;
-            if (tipo == 1) {
-                salarioFinal = getSalario();
-            } else if (tipo == 2) {
-                salarioFinal = getSalario() + getSalario() * getPercentualComissao();
-            } else if (tipo == 3) {
-                salarioFinal = getSalario() + getSalario() + getPercentualComissao() + getBonus();
-            } else if (tipo == 4) {
-                salarioFinal = getBonus();
-            }
-            System.out.println(String.format("%.2f", salarioFinal));
-    }*/
     public void salarioFinal(){
         double salarioFinal = getSalario();
+        System.out.println("O salario final e: " + salarioFinal);
     }
 }
---
+++ src/EmpregadoQuatro.java
@@ -0,0 +1,40 @@
+
+/**
+ *
+ * @author Aline
+ */
+
+public class EmpregadoQuatro extends Empregado{
+    
+    //atributos exclusivos
+    private double bonus;
+    
+    //construtor
+    public EmpregadoQuatro(){
+    
+    }
+    
+    public EmpregadoQuatro(double bonus){
+        this.bonus = bonus;
+    }
+    
+    //get e set exclusivo
+    public double getBonus(){
+        return bonus;
+    }
+    
+    public void setBonus(double bonus){
+        this.bonus = bonus;
+    }
+    
+    @Override
+    public void salarioFinal(){
+        double salarioFinal = getBonus();
+        System.out.println("O salario final e: " + salarioFinal);
+    }
+    
+    public void aumentaSalario(double bonus){
+        setBonus(2000 + bonus * 2500);
+    }
+    
+}
